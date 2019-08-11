0e46eb42526bc9b094171ff0a35ed7b715fbe41f
Imran Iqbal <imran1337@gmail.com>
2019-08-11 14:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
-  return object
+  object[key] = value;
+  return object;
 }
