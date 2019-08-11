3aae02fe615a97fd7193d20f8ecba70dc3e620c6
Imran Iqbal <imran1337@gmail.com>
2019-08-11 11:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var object = { key: value }
-  var newObj = Object.assign({}, object)
+  object[key] = value
+  
   return newObj
 }
