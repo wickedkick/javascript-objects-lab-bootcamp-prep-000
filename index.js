30abc1d341203b1cece97bc86437f58e12563a69
Imran Iqbal <imran1337@gmail.com>
2019-08-11 14:52:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   return Object.assign({}, { [object]: key })
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
